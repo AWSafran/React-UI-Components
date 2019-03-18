89d9a2698da93c2702c9f731ea0286980a153294
AWSafran <andrewsafran@gmail.com>
2019-03-18 15:55:53 -0400

    created components and added them to app.js for all mvp required features

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = () => {
+    return <p className="bannerContent">Begin by exploring the `components` directory. You'll notice we have a few files that we've already included in there to get you started right away building components. You'll need to make sure you include your components that you build in this file to watch your app come to life</p>;
+}
+
+export default CardContent
